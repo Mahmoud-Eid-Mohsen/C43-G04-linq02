b412f717a27f556361e304a58ce568d6bb36c84a
admin <admin@DESKTOP-JKD834T>
2025-02-10 19:36:38 +0200

    Element Operators

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Assigment
+﻿using Assigment.Data;
+using System.Diagnostics;
+using  static Assigment.Data.ListGenerator;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Assigment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Element Operators
+            //1. Get first Product out of Stock 
+
+
+            var result = ProductsList.FirstOrDefault(p=>p.UnitsInStock==0);
+
+
+           // 2.Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            var Result = ProductsList.FirstOrDefault(p => p.UnitPrice>1000);
+
+
+           // 3.Retrieve the second number greater than 5
+             int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var ResulT = Arr.Where(p => p > 5).ElementAtOrDefault(1);
+            
+                #endregion
         }
     }
 }
